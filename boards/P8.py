dabcb7e72a7bf0879c1320c37f8b4419c514fc14
SpyX <66248814+spyx1@users.noreply.github.com>
2024-10-25 10:37:38 +0300

    Update P8.py

--- boards/P8.py
+++ boards/P8.py
@@ -26,35 +26,27 @@ info = {
 # 'default_console_tx' : "D6",
 # 'default_console_rx' : "D8",
 # 'default_console_baudrate' : "38400",
- 'variables' : 2565, # 2565 SD5.0 0x200014B8 SD 3.0 0x200019C0  How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
+ 'variables' : 2565, # SD5.0 0x200014B8 SD 3.0 0x200019C0  How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
  'bootloader' : 1,
- 'binary_name' : 'espruino_%v_p8_SDK14.hex',
+ 'binary_name' : 'espruino_%v_p8_SDK12_SD30.hex',
  'build' : {
    'optimizeflags' : '-Os',
    'libraries' : [
      'BLUETOOTH',
 #     'NET',
      'GRAPHICS',
-#     'LCD_SPI',
 #     'NFC',
 #     'NEOPIXEL'
    ],
    'makefile' : [
 #    'SAVE_ON_FLASH=1',
 #     'DEFINES+=-DCONFIG_GPIO_AS_PINRESET', # Allow the reset pin to work
-     'DEFINES+=-DBLUETOOTH_NAME_PREFIX=\'"P8"\'',
-#     'DEFINES += -DUSE_DEBUGGER -DUSE_TAB_COMPLETE',
-     'DEFINES+=-DUSE_FONT_6X8 -DGRAPHICS_PALETTED_IMAGES=1 -DGRAPHICS_FAST_PATHS=1',
-     'DEFINES += -DRNG_CONFIG_POOL_SIZE=64 -DSPI0_USE_EASY_DMA=1',
+     'DEFINES+= -DUSE_FONT_6X8 -DBLE_HIDS_ENABLED=1 -DBLUETOOTH_NAME_PREFIX=\'"P8"\'',
+     'USE_LCD_SPI_UNBUF=1',
+     'DEFINES+= -DSPISENDMANY_BUFFER_SIZE=126',
      'DFU_PRIVATE_KEY=targets/nrf5x_dfu/dfu_private_key.pem',
-     'DFU_SETTINGS=--application-version 0xff --hw-version 52 --sd-req 0x9d',
-
-#     'INCLUDE += -I$(ROOT)/libs/banglejs -I$(ROOT)/libs/misc',
-#     'WRAPPERSOURCES += libs/banglejs/jswrap_banglep8.c',
-# -DUSE_HEATSHRINK',
-#     'DEFINES += -DUART1_ENABLED=0',
-     'DEFINES += -DFDS_VIRTUAL_PAGES=2',
-     'NRF_SDK14=1',
+     'NRF_BL_DFU_INSECURE=1',
+     'DFU_SETTINGS=--application-version 0xff --hw-version 52 --sd-req 0x8C,0x91'
    ]
  }
 };
@@ -74,23 +66,18 @@ chip = {
   'dac' : 0,
   'saved_code' : {
     'page_size' : 4096,
-#storage in builtin flash
-    'address' : ((118 - 16) * 4096), # Bootloader takes pages 120-127, FS takes 118-119
-    'pages' : 16,
-    'flash_available' : 512 - ((35 + 8 + 2 + 16)*4) # Softdevice 5.0  uses 35 pages of flash, bootloader 8, FS 2, no code. Each page is 4 kb.
-# storage in SPI flash
-#    'address' : 0x60320000,
-#    'pages' : 96, #  0x60320000-0x60380000 - looks like unused space - all FFs
-#    'flash_available' : 512 - ((35 + 8 + 2 + 0)*4) # Softdevice 5.0  uses 35 pages of flash, bootloader 8, FS 2, no code. Each page is 4 kb.
-
-#    'flash_available' : 512 - ((28 + 8 + 2 + 10)*4) # Softdevice 2.0 uses 28 pages of flash, bootloader 8, FS 2, code 10. Each page is 4 kb.
+    'address' : ((118 - 20) * 4096), # Bootloader takes pages 120-127, FS takes 118-119
+    'pages' : 20,
+    'flash_available' : 512 - ((31 + 8 + 2 + 20)*4) # Softdevice 2.0 uses 28 pages of flash, bootloader 8, FS 2, code 10. Each page is 4 kb.
+#    'address' : 0x60380000, # Bootloader takes pages 120-127, FS takes 118-119
+#    'pages' : 32,
 #    'flash_available' : 512 - ((31 + 8 + 2 + 10)*4) # Softdevice 3.0 uses 31 pages of flash, bootloader 8, FS 2, code 10. Each page is 4 kb.
-#    'flash_available' : 512 - ((35 + 8 + 10 + 10)*4) # Softdevice 5.0  uses 35 pages of flash, bootloader 8, FS 2, code 10. Each page is 4 kb.
+#    'flash_available' : 512 - ((35 + 8 + 2 + 16)*4) # Softdevice 5.0  uses 35 pages of flash, bootloader 8, FS 2, code 10. Each page is 4 kb.
   },
 };
 
 devices = {
-  'BTN1' : { 'pin' : 'D17', 'pinstate' : 'IN_PULLDOWN' },
+ 'BTN1' : { 'pin' : 'D17', 'pinstate' : 'IN_PULLDOWN' },
 #  'BTN2' : { 'pin' : 'D14', 'pinstate' : 'IN_PULLDOWN' }, # Pin negated in software
 #  'BTN3' : { 'pin' : 'D15', 'pinstate' : 'IN_PULLDOWN' }, # Pin negated in software
 #  'BTN4' : { 'pin' : 'D16', 'pinstate' : 'IN_PULLDOWN' }, # Pin negated in software
@@ -98,10 +85,6 @@ devices = {
 #  'LED2' : { 'pin' : 'D18' }, # Pin negated in software
 #  'LED3' : { 'pin' : 'D19' }, # Pin negated in software
 #  'LED4' : { 'pin' : 'D20' }, # Pin negated in software
-#  'RX_PIN_NUMBER' : { 'pin' : 'D8'},
-#  'TX_PIN_NUMBER' : { 'pin' : 'D6'},
-#  'CTS_PIN_NUMBER' : { 'pin' : 'D7'},
-#  'RTS_PIN_NUMBER' : { 'pin' : 'D5'},
   # Pin D22 is used for clock when driving neopixels - as not specifying a pin seems to break things
   'VIBRATE' : { 'pin' : 'D16' }, # Pin negated in software
   'BAT' : {
@@ -109,25 +92,11 @@ devices = {
             'pin_voltage' : 'D31'
           },
 
-#  'LCD' : {
-#            'width' : 240, 'height' : 240, 'bpp' : 4,
-#            'controller' : 'st7789v',
-#            'pin_dc' : 'D18',
-#            'pin_cs' : 'D25',
-#            'pin_rst' : 'D26',
-#            'pin_sck' : 'D2',
-#            'pin_mosi' : 'D3',
-#            'pin_bl' : 'D14',
-#          },
-
   'SPIFLASH' : {
             'pin_sck' : 'D2',
             'pin_mosi' : 'D3',
             'pin_miso' : 'D4',
             'pin_cs' : 'D5',
-##            'pin_wp' : 'D22',
-##            'pin_hold' : 'D23',
-##            'pin_rst' : 'D23', # no reset but this is HOLD pin for XENON, we want it set to 1
             'size' : 4096*1024, # 4MB
             'memmap_base' : 0x60000000 # map into the address space (in software)
   }
