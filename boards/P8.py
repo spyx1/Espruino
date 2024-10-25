f5d0e2788a5b7895aca601d61a255ed8ea13a72c
SpyX <66248814+spyx1@users.noreply.github.com>
2024-10-25 08:18:29 +0300

    Update P8.py

--- boards/P8.py
+++ boards/P8.py
@@ -30,6 +30,7 @@ info = {
      'GRAPHICS',
 #     'NFC',
 #     'NEOPIXEL'
+    'LCD_SPI_UNBUF',
    ],
    'makefile' : [
 #    'SAVE_ON_FLASH=1',
@@ -39,6 +40,7 @@ info = {
      'DEFINES+= -DUSE_FONT_6X8 -DBLE_HIDS_ENABLED=1 -DBLUETOOTH_NAME_PREFIX=\'"P8"\'',
      'DEFINES+= -DSPISENDMANY_BUFFER_SIZE=126',
      'DEFINES += -DSPIFLASH_SHARED_SPI',
+     'USE_LCD_SPI_UNBUF=1',
      'DFU_PRIVATE_KEY=targets/nrf5x_dfu/dfu_private_key.pem',
      'NRF_BL_DFU_INSECURE=1',
      'DFU_SETTINGS=--application-version 0xff --hw-version 52 --sd-req 0x8C,0x91'
